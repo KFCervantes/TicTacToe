6599d2f899c43a6fa26ccb40985c573969b9c986
KFCervantes <kalebfcervantes@gmail.com>
2017-07-19 21:53:28 -0700

    added "game over" messages highlights the player's character

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -25,6 +25,10 @@ class ViewController: UIViewController {
 	var player: Character = "X"
 	var cpu: Character = "O"
 	
+	let draw_img = UIImage(named: "draw.png")
+	let lose_img = UIImage(named: "lose.png")
+	let highlight_img = UIImage(named: "highlight.png")
+	
 	@IBOutlet weak var button_0: UIButton!
 	@IBOutlet weak var button_1: UIButton!
 	@IBOutlet weak var button_2: UIButton!
@@ -34,6 +38,11 @@ class ViewController: UIViewController {
 	@IBOutlet weak var button_6: UIButton!
 	@IBOutlet weak var button_7: UIButton!
 	@IBOutlet weak var button_8: UIButton!
+	
+	@IBOutlet weak var game_over_message: UIImageView!
+	
+	@IBOutlet weak var select_x: UIButton!
+	@IBOutlet weak var select_o: UIButton!
 	
 	@IBAction func player_move(_ sender: UIButton) {
 		if !wrapper.game_over(board) {
@@ -46,30 +55,51 @@ class ViewController: UIViewController {
 				board.change_at(player_move_index, with: player)
 			}
 			
-			let cpu_str = String(cpu)
-			
-			if (!wrapper.game_over(board) && (wrapper.whose_turn(board) == cpu_str)) {
-				var buttons: [UIButton] = [
-					button_0,
-					button_1,
-					button_2,
-					button_3,
-					button_4,
-					button_5,
-					button_6,
-					button_7,
-					button_8
-				]
-				let cpu_move_index = Int(wrapper.next_best_move_index(board))
-				let cpu_char_file = cpu_str + ".png"
-				let cpu_char_img = UIImage(named: cpu_char_file)
-				buttons[cpu_move_index].setImage(cpu_char_img, for: .normal)
-				board.change_at(cpu_move_index, with: cpu)
+			if !wrapper.game_over(board) {
+				let cpu_str = String(cpu)
+				
+				if (wrapper.whose_turn(board) == cpu_str){
+					var buttons: [UIButton] = [
+						button_0,
+						button_1,
+						button_2,
+						button_3,
+						button_4,
+						button_5,
+						button_6,
+						button_7,
+						button_8
+					]
+					let cpu_move_index = Int(wrapper.next_best_move_index(board))
+					let cpu_char_file = cpu_str + ".png"
+					let cpu_char_img = UIImage(named: cpu_char_file)
+					buttons[cpu_move_index].setImage(cpu_char_img, for: .normal)
+					board.change_at(cpu_move_index, with: cpu)
+					
+					if wrapper.game_over(board){
+						if board.contains(" ") {
+							game_over_message.image = lose_img
+						}
+						else {
+							game_over_message.image = draw_img
+						}
+					}
+				}
+			}
+			else if board.contains(" ") {
+				game_over_message.image = lose_img
+			}
+			else {
+				game_over_message.image = draw_img
 			}
 		}
 	}
 	
 	@IBAction func x_reset(_ sender: UIButton) {
+		sender.setBackgroundImage(highlight_img, for: .normal)
+		select_o.setBackgroundImage(nil, for: .normal)
+		game_over_message.image = nil
+
 		player = "X"
 		cpu = "O"
 		if board != "         " {
@@ -94,6 +124,10 @@ class ViewController: UIViewController {
 	}
 	
 	@IBAction func o_reset(_ sender: UIButton) {
+		sender.setBackgroundImage(highlight_img, for: .normal)
+		select_x.setBackgroundImage(nil, for: .normal)
+		game_over_message.image = nil
+		
 		player = "O"
 		cpu = "X"
 		var buttons: [UIButton] = [
@@ -114,6 +148,7 @@ class ViewController: UIViewController {
 			
 			board = "         "
 		}
+		
 		let x_index = Int(wrapper.next_best_move_index(board))
 		let x_img = UIImage(named: "X.png")
 		buttons[x_index].setImage(x_img, for: .normal)
